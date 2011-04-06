0e29cb680ac341b2f86dddb5e916e93d2602f58f
Sten Roger Sandvik <srs@enonic.com>
2011-04-06 18:45:41 +0200

    Fixed version number so that it prints out timestamp.

--- modules/cms-api/src/main/java/com/enonic/cms/api/Version.java
+++ modules/cms-api/src/main/java/com/enonic/cms/api/Version.java
@@ -5,18 +5,19 @@
 
 package com.enonic.cms.api;
 
-import java.util.regex.Pattern;
+import java.io.InputStream;
+import java.util.Properties;
 
 public final class Version
 {
+    private final static Properties PROPS =
+        loadProperties();
+
     private final static String TITLE =
         "Enonic CMS";
     
     private final static String COPYRIGHT =
-        "Copyright (c) 2000-2011 Enonic AS.";
-
-    private final static String VERSION =
-        Version.class.getPackage().getImplementationVersion();
+        "Copyright (c) 2000-2011 Enonic AS";
 
     /**
      * Return the version title.
@@ -39,7 +40,14 @@ public final class Version
      */
     public static String getVersion()
     {
-        return VERSION != null ? VERSION : "x.x.x";
+        final String version = PROPS.getProperty("version", "x.x.x");
+        final String timestamp = getTimestamp();
+
+        if ((timestamp != null) && version.endsWith("-SNAPSHOT")) {
+            return version.replaceAll("-SNAPSHOT", "-" + timestamp);
+        }
+
+        return version;
     }
 
     /**
@@ -50,11 +58,33 @@ public final class Version
         return getTitle() + " " + getVersion();
     }
 
+    public static String getTimestamp()
+    {
+        return PROPS.getProperty("timestamp", null);
+    }
+
     /**
      * Print out version.
      */
     public static void main( String[] args )
     {
         System.out.println( getTitleAndVersion() );
+    }
+
+    private static Properties loadProperties()
+    {
+        final Properties props = new Properties();
+        final InputStream in = Version.class.getResourceAsStream("version.properties");
+        if (in == null) {
+            return props;
+        }
+
+        try {
+            props.load(in);
+        } catch (Exception e) {
+            // Do nothing
+        }
+
+        return props;
     }
 }
