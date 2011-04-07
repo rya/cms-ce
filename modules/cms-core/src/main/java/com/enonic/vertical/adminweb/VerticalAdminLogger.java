6d13b988b02d858bb9706a5c95a7c927a17808fc
Sten Roger Sandvik <srs@enonic.com>
2011-04-07 20:56:42 +0200

    Removed unused classes.

--- modules/cms-core/src/main/java/com/enonic/vertical/adminweb/VerticalAdminLogger.java
+++ modules/cms-core/src/main/java/com/enonic/vertical/adminweb/VerticalAdminLogger.java
@@ -5,22 +5,8 @@
 package com.enonic.vertical.adminweb;
 
 import com.enonic.vertical.VerticalLogger;
-import com.enonic.vertical.VerticalRuntimeException;
 
 public class VerticalAdminLogger
     extends VerticalLogger
 {
-    public static void fatalAdmin( Class clazz, int msgKey, String message, Throwable throwable )
-        throws VerticalAdminRuntimeException
-    {
-
-        try
-        {
-            fatal( clazz, msgKey, message, throwable, VerticalAdminRuntimeException.class );
-        }
-        catch ( VerticalRuntimeException vre )
-        {
-            throw (VerticalAdminRuntimeException) vre;
-        }
-    }
 }
