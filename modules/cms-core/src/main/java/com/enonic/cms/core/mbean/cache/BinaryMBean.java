92a7f000a8b878d64252df5e68940548d86e6111
Viktar Fiodarau <vfi@enonic.com>
2011-04-08 12:10:43 +0300

    B-01836: Move service.mbean to core.mbean Move com.enonic.cms.server.service.mbean to com.enonic.cms.core.mbean.

--- modules/cms-core/src/main/java/com/enonic/cms/core/mbean/cache/BinaryMBean.java
+++ modules/cms-core/src/main/java/com/enonic/cms/core/mbean/cache/BinaryMBean.java
@@ -2,9 +2,9 @@
  * Copyright 2000-2011 Enonic AS
  * http://www.enonic.com/license
  */
-package com.enonic.cms.server.service.mbean.cache;
+package com.enonic.cms.core.mbean.cache;
 
-public interface XsltMBean
+public interface BinaryMBean
     extends Cache
 {
 }
