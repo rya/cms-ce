5aec3e4f09780d8b8c68d40e89ff77b083ac5fd7
Sten Roger Sandvik <srs@enonic.com>
2011-04-13 17:25:15 +0200

    Removed tool pages. Will be included in new admin.

--- modules/cms-core/src/main/java/com/enonic/cms/core/tools/WelcomeController.java
+++ modules/cms-core/src/main/java/com/enonic/cms/core/tools/WelcomeController.java
@@ -4,33 +4,21 @@
  */
 package com.enonic.cms.core.tools;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.support.JdbcUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
-import com.enonic.cms.framework.jdbc.dialect.Dialect;
-import com.enonic.cms.framework.jdbc.dialect.SqlServerDialect;
-
 import com.enonic.cms.api.Version;
 import com.enonic.cms.core.structure.SiteService;
-import com.enonic.cms.store.support.ConnectionFactory;
-import com.enonic.cms.upgrade.UpgradeService;
 
 import com.enonic.cms.core.structure.SiteEntity;
 
@@ -43,44 +31,12 @@ import com.enonic.cms.core.structure.SiteEntity;
 public final class WelcomeController
     extends AbstractController
 {
-    private UpgradeService upgradeService;
-
-    private ToolsAccessResolver toolsAccessResolver;
-
-    private Dialect dialect;
-
-    private ConnectionFactory connectionFactory;
-
     private SiteService siteService;
 
     @Autowired
     public void setSiteService( SiteService siteService )
     {
         this.siteService = siteService;
-    }
-
-    @Autowired
-    public void setUpgradeService( final UpgradeService upgradeService )
-    {
-        this.upgradeService = upgradeService;
-    }
-
-    @Autowired
-    public void setToolsAccessResolver( final ToolsAccessResolver toolsAccessResolver )
-    {
-        this.toolsAccessResolver = toolsAccessResolver;
-    }
-
-    @Autowired
-    public void setDialect( final Dialect dialect )
-    {
-        this.dialect = dialect;
-    }
-
-    @Autowired
-    public void setConnectionFactory( final ConnectionFactory connectionFactory )
-    {
-        this.connectionFactory = connectionFactory;
     }
 
     private Map<String, Integer> createSiteMap()
@@ -97,87 +53,15 @@ public final class WelcomeController
         return siteMap;
     }
 
-    private Set<Map.Entry<String, String>> createAdditionalMessages( boolean upgradeNeeded )
-    {
-        final Map<String, String> msgList = new HashMap<String, String>();
-        if ( upgradeNeeded )
-        {
-            checkUseOfDataTypeWhenRunningMSSQLServer( msgList );
-        }
-        return msgList.entrySet();
-    }
-
-    private void checkUseOfDataTypeWhenRunningMSSQLServer( final Map<String, String> msgList )
-    {
-        try
-        {
-            if ( dialect instanceof SqlServerDialect && !currentDatabaseUseNvarcharDataType() )
-            {
-                StringBuilder builder = new StringBuilder();
-                builder.append(
-                    "If varchar is used instead of nvarchar you will not be able to use unicode characters throughout Enonic CMS. " );
-                builder.append(
-                    "Note that due to the different java versions and/or jdbc driver type versions this information might not be accurate. " );
-                builder.append( "Please verify that the appropriate datatype is used. " );
-                builder.append(
-                    "To convert from varchar to nvarchar you may use the dbtool to do a backup/restore. Please consult the documentation." );
-
-                msgList.put( "MSSql Server Warning", builder.toString() );
-            }
-        }
-        catch ( SQLException ex )
-        {
-            msgList.put( "MSSql Server Warning!", "Could not determine use of varchar/nvarchar" );
-        }
-    }
-
-    private boolean currentDatabaseUseNvarcharDataType()
-        throws SQLException
-    {
-        Statement stmt = null;
-        ResultSet result = null;
-        Connection connection = null;
-
-        try
-        {
-            connection = connectionFactory.getConnection( true );
-            stmt = connection.createStatement();
-            result = stmt.executeQuery( "select mve_sKey from tModelVersion" );
-            final ResultSetMetaData metaData = result.getMetaData();
-            return metaData.getColumnType( 1 ) == -9; // NVARCHAR
-        }
-        finally
-        {
-            JdbcUtils.closeResultSet( result );
-            JdbcUtils.closeStatement( stmt );
-            JdbcUtils.closeConnection( connection );
-        }
-    }
-
     protected ModelAndView handleRequestInternal( HttpServletRequest req, HttpServletResponse res )
         throws Exception
     {
-        final boolean modelUpgradeNeeded = this.upgradeService.needsUpgrade();
-        final boolean softwareUpgradeNeeded = this.upgradeService.needsSoftwareUpgrade();
-        final boolean upgradeNeeded = modelUpgradeNeeded || softwareUpgradeNeeded;
-
         HashMap<String, Object> model = new HashMap<String, Object>();
         model.put( "versionTitle", Version.getTitle() );
         model.put( "versionTitleVersion", Version.getTitleAndVersion() );
         model.put( "versionCopyright", Version.getCopyright() );
         model.put( "baseUrl", createBaseUrl( req ) );
-        if ( !upgradeNeeded )
-        {
-            model.put( "sites", createSiteMap() );
-        }
-        model.put( "upgradeNeeded", upgradeNeeded );
-        model.put( "modelUpgradeNeeded", modelUpgradeNeeded );
-        model.put( "softwareUpgradeNeeded", softwareUpgradeNeeded );
-        model.put( "upgradeFrom", this.upgradeService.getCurrentModelNumber() );
-        model.put( "upgradeTo", this.upgradeService.getTargetModelNumber() );
-        model.put( "toolsRestricted", !this.toolsAccessResolver.hasAccess( req ) );
-        model.put( "toolsRestrictedError", this.toolsAccessResolver.getErrorMessage( req ) );
-        model.put( "additionalMessages", createAdditionalMessages( upgradeNeeded ) );
+        model.put( "sites", createSiteMap() );
         return new ModelAndView( "welcomePage", model );
     }
 
--- modules/cms-webapp/src/main/webapp/WEB-INF/freemarker/welcomePage.ftl
+++ modules/cms-webapp/src/main/webapp/WEB-INF/freemarker/welcomePage.ftl
@@ -48,52 +48,20 @@
 <body>
 <h1>Welcome to ${versionTitleVersion}</h1>
 
-[#if modelUpgradeNeeded == true]
-<div class="infoBoxError">
-    <b>Upgrade Needed!</b>
-    <br/>
-    Database upgrade from model <b>${upgradeFrom}</b> to model <b>${upgradeTo}</b> is needed. Admin or site will not
-    work correctly if not upgraded. Go to <a href="${baseUrl}/tools/upgrade">tools/upgrade</a> to upgrade.
-</div>
-[/#if]
-[#if softwareUpgradeNeeded == true]
-<div class="infoBoxError">
-    <b>Software Upgrade Needed!</b>
-    <br/>
-    Database model is newer than software allows. Please upgrade the software. Admin or site will not
-    work correctly if not upgraded.
-</div>
-[/#if]
-[#list additionalMessages as message]
-<div class="infoBoxWarning">
-    <b>${message.key}</b>
-    <br/>
-${message.value}
-</div>
-[/#list]
-
 <div class="infoBox">
     <b>Management components</b>
     <ul>
-        <li><a href="${baseUrl}/admin/">Admin Console</a></li>
         <li><a href="${baseUrl}/dav">WebDav Location</a></li>
     </ul>
 </div>
 <div class="infoBox">
     <b>Sites</b>
-[#if upgradeNeeded == false]
     <ul>
         [#list sites?keys?sort as key]
             <li><a href="${baseUrl}/site/${sites[key]}">${key}</a> <!--(${sites[key]})--></li>
         [/#list]
 
     </ul>
-[/#if]
-[#if upgradeNeeded == true]
-    <p>
-        N/A
-    </p>
-[/#if]
 </div>
 <div class="infoBox">
     <b>RPC endpoints</b>
@@ -102,17 +70,5 @@ ${message.value}
     </ul>
 </div>
 
-<div class="${toolsRestricted?string('infoBoxError', 'infoBox')}">
-    <b>Tools</b>
-[#if toolsRestricted == true]
-    <i>(${toolsRestrictedError})</i>
-[/#if]
-    <ul>
-        <li><a href="${baseUrl}/tools/properties">Configuration Properties</a></li>
-        <li><a href="${baseUrl}/tools/upgrade">Upgrade Tool</a></li>
-        <li><a href="${baseUrl}/tools/connectioninfo">Connection Information</a></li>
-        <li><a href="${baseUrl}/tools/liveportaltrace">Live Portal Trace</a></li>
-    </ul>
-</div>
 </body>
 </html>
