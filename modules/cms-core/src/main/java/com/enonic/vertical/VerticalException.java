e13e09fd5371a2cb43ebc41fd946431add5277c8
Sten Roger Sandvik <srs@enonic.com>
2011-04-18 16:09:55 +0200

    Removed some unused code.

--- modules/cms-core/src/main/java/com/enonic/vertical/VerticalException.java
+++ modules/cms-core/src/main/java/com/enonic/vertical/VerticalException.java
@@ -4,10 +4,6 @@
  */
 package com.enonic.vertical;
 
-import org.slf4j.LoggerFactory;
-
-import java.lang.reflect.Constructor;
-
 /**
  * Root exception for all Vertical exceptions.
  */
@@ -15,48 +11,10 @@ public class VerticalException
         extends RuntimeException
 {
     /**
-     * Message key.
-     */
-    private int messageKey;
-
-    /**
      * Construct the exception.
      */
     public VerticalException( String message )
     {
         super( message );
     }
-
-    /**
-     * Construct the exception.
-     */
-    public VerticalException( String message, Throwable cause )
-    {
-        super( message, cause );
-    }
-
-    /**
-     * Construct the exception.
-     */
-    public VerticalException( Throwable cause )
-    {
-        super( cause.getMessage(), cause );
-    }
-
-    /**
-     * Return the message key.
-     */
-    public int getMessageKey()
-    {
-        return this.messageKey;
-    }
-
-    /**
-     * Set message key.
-     */
-    public void setMessageKey( int messageKey )
-    {
-        this.messageKey = messageKey;
-    }
-
 }
