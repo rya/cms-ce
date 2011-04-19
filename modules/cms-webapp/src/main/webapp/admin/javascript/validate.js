2ae571628aeee48744e6cae112733a1e1315519c
Thomas Andersen <tan@enonic.com>
2011-04-19 10:38:38 +0200

    D-01755: Content: When using "length" as input name, the javascript validation fails - Fixed a bad case where the element index in form.elements pointed to the element name

--- modules/cms-webapp/src/main/webapp/admin/javascript/validate.js
+++ modules/cms-webapp/src/main/webapp/admin/javascript/validate.js
@@ -356,6 +356,7 @@ function isEmpty(e)
 function error(element, msg, tabPane)
 {
     alert(msg);
+
     if (!element)
         return;
 
@@ -367,21 +368,20 @@ function error(element, msg, tabPane)
                 tabPane.setSelectedPage( _objTabPage.id );
                         
         }
-
-        moveFocusTo( element );
     }
     catch (e){
 
         if( tabPane != null ) {
             // get tabPageId to the page where the form element is.
+
             var objTabPage = getParentNodeOfClass( element, "tab-page" );
             // move the pane to the right page
             tabPane.setSelectedPage( objTabPage.id );
             moveFocusTo( element );
         }
 
-        moveFocusTo( element );
     }
+    moveFocusTo( element );
 }
 
 function findPos(obj) {
@@ -993,6 +993,8 @@ function validateRelatedContent(e, fieldname, tabPane) {
 function validateRequired(e, fieldname, tabPane, ignoreFirstPos) {
   var i;
 
+
+
   if (isArray(e)) {
     // element er et kobble med related content ( e[i].value )
     for( i = 0; i < e.length; i++ ) {
@@ -1129,14 +1131,14 @@ function isArray( obj )
 
 function checkAll(formName, fields, tabPane)
 {
-  var i;
+    var i;
 	var f = document.forms[formName];
 
 	for (i = 0; i < fields.length; i++)
 	{
 		if ( fields[i] != null)
 		{
-		    var element = f.elements[fields[i][1]];
+		    var element = f[fields[i][1]];
 	        var elementCaption = fields[i][0];
 	        var regExp;
 
