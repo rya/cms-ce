ea463a18b4a37658840976ee3012fd09b8c41d4a
Sten Roger Sandvik <srs@enonic.com>
2011-04-27 13:37:51 +0200

    Fixed back the global config test to use DefaultConverterService.

--- modules/cms-core/src/test/java/com/enonic/cms/core/config/GlobalConfigImplTest.java
+++ modules/cms-core/src/test/java/com/enonic/cms/core/config/GlobalConfigImplTest.java
@@ -2,10 +2,8 @@ package com.enonic.cms.core.config;
 
 import com.google.common.collect.Maps;
 import org.junit.Test;
-import org.springframework.core.convert.support.GenericConversionService;
-
+import org.springframework.core.convert.support.DefaultConversionService;
 import java.util.Map;
-import java.util.Properties;
 import static org.junit.Assert.*;
 
 public class GlobalConfigImplTest
@@ -31,12 +29,12 @@ public class GlobalConfigImplTest
 
     private GlobalConfigImpl build()
     {
-        GenericConversionService converterService = new GenericConversionService();
-        converterService.addConverter(new String2FileConverter());
+        final DefaultConversionService converter = new DefaultConversionService();
+
         final Map<String, String> props = Maps.newHashMap();
         props.put("cms.home", "/some/folder");
         props.put("cms.xsl.maxRecursionDepth", "1");
 
-        return new GlobalConfigImpl(converterService, props);
+        return new GlobalConfigImpl(converter, props);
     }
 }
