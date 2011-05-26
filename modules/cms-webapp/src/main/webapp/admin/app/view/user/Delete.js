4513b8de6ba50003eeb201a831514af5dd1b95c1
Viktar Fiodarau <vfi@enonic.com>
2011-05-26 15:20:18 +0300

    B-01993: Admin 2.0: Accounts: Create delete user dialog See layout here:
    http://wiki.enonic.com/display/dev/Accounts+Browse
    
    - Remove the window - close button
    
    - When pressing the cancel button the dialog should be closed
    
    - When pressing the delete button: alert "Not implemented"

--- modules/cms-webapp/src/main/webapp/admin/app/view/user/Delete.js
+++ modules/cms-webapp/src/main/webapp/admin/app/view/user/Delete.js
@@ -1,4 +1,4 @@
-Ext.define('CMS.view.user.Delete', {
+Ext.define( 'CMS.view.user.Delete', {
     extend: 'Ext.window.Window',
     alias: 'widget.userDeleteWindow',
 
@@ -8,14 +8,75 @@ Ext.define('CMS.view.user.Delete', {
     title: 'Delete User',
     modal: true,
 
-    initComponent: function() {
-        this.callParent(arguments);
+    layout: {
+        type: 'vbox',
+        align: 'stretch',
+        defaultMargins: {top:10, right:10, bottom:10, left:10}
     },
 
-    doShow: function(model) {
+    items: [
+        {
+            xtype: 'container',
+            id: 'line1',
+            layout: {
+                type: 'hbox',
+                pack: 'left',
+                defaultMargins: {top:0, right:10, bottom:0, left:0}
+            },
+            items: [
+                {
+                    xtype: 'image',
+                    id: 'photo'
+                },
+                {
+                    xtype: 'label',
+                    id: 'dName'
+                }
+            ]
+        },
+        {
+            xtype: 'container',
+            layout: {
+                type: 'hbox',
+                align: 'stretch',
+                pack: 'center',
+                defaultMargins: {top:0, right:110, bottom:0, left:50}
+            },
+            height: 20,
+            items: [
+                {
+                    xtype: 'button',
+                    text: 'Cancel',
+                    handler: function(b, e)
+                    {
+                        b.findParentByType('window').close();
+                    }
+                },
+                {
+                    xtype: 'button',
+                    text: 'Delete',
+                    handler: function()
+                    {
+                        alert( 'Not implemented' );
+                    }
+                }
+            ]
+        }
+    ],
+
+    initComponent: function()
+    {
+        this.callParent( arguments );
+
+    },
+
+    doShow: function( model )
+    {
         this.title = 'Delete User >> ' + model.data.displayName;
+        this.child( '#line1' ).child( '#dName' ).text = model.data.displayName + ' (' + model.data.qualifiedName + ')';
+        this.child( '#line1' ).child( '#photo' ).src = 'rest/users/' + model.data.key + '/photo';
         this.show();
     }
 
-});
+} );
 
