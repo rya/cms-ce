6e1292a94fd2734428c5f91d5864a0c91eff7a61
Runar Myklebust <rmy@enonic.com>
2011-08-09 10:50:30 +0200

    D-01828: Remote JAVA API: UnassignContent and AssignContent fails with exception
    Missing serializable-declarations. Added for all params where this was missing.

--- modules/cms-api/src/main/java/com/enonic/cms/api/client/model/UnassignContentParams.java
+++ modules/cms-api/src/main/java/com/enonic/cms/api/client/model/UnassignContentParams.java
@@ -9,7 +9,10 @@ package com.enonic.cms.api.client.model;
  * Jun 10, 2010
  */
 public class UnassignContentParams
+    extends AbstractParams
 {
+    private static final long serialVersionUID = 6457109901570171969L;
+
     /**
      * The key of the content to unassign.
      */
