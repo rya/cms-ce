8792e92726a7b3bcf0f804827eed495bfc0d1559
Viktar Fiodarau <vfi@enonic.com>
2011-08-17 17:48:26 +0300

    B-02177: Admin 2.0: Accounts: User Preview Pane - multiple selection
    Update the detail panel in the Browse Accounts screen according to the spec in this wiki page:
    http://wiki.enonic.com/display/dev/Task+-+Accounts+Preview+Pane+-+Multiselect
    
    This will be the user preview panel when more than one user are selected.
    
    This task should not be implemented until B-02176 (single selection) is completed.

---
+++ modules/cms-webapp/src/main/webapp/admin/app/view/user/UserDetailButton.js
@@ -0,0 +1,86 @@
+Ext.define('CMS.view.user.UserDetailButton', {
+    extend: 'Ext.panel.Panel',
+    alias: 'widget.userDetailButton',
+
+    layout: 'column',
+    margin: 5,
+    bodyStyle: {
+        background: 'lightGrey'
+    },
+
+    initComponent: function(){
+        var displayNamePane = {
+            xtype: 'panel',
+            border: 0,
+            height: 25,
+            layout: 'fit',
+            bodyStyle: {
+                background: 'lightGrey'
+            },
+            items: [
+                {
+                    xtype: 'displayfield',
+                    style: {
+                        background: 'lightGrey'
+                    },
+                    cls: 'display-name',
+                    value: this.userData.get('displayName')
+                }
+            ]
+        };
+        var qNamePane = {
+            xtype: 'panel',
+            border: 0,
+            height: 15,
+            layout: 'fit',
+            items: [
+                {
+                    xtype: 'displayfield',
+                    style: {
+                        background: 'lightGrey'
+                    },
+                    value: '(' + this.userData.get('userStore') + '/' + this.userData.get('name') + ')'
+                }
+            ]
+        };
+        var imagePane = {
+            xtype: 'panel',
+            layout: 'fit',
+            border: 0,
+            items: [
+                {
+                    xtype: 'image',
+                    style: {
+                        background: 'lightGrey'
+                    },
+                    src: 'data/user/photo?key=' + this.userData.get('key') + '&thumb=true',
+                    padding: 5
+                }]
+        };
+        var namePane = {
+            xtype: 'panel',
+            border: 0,
+            padding: 5,
+            bodyStyle: {
+                background: 'lightGrey'
+            },
+            items: [displayNamePane, qNamePane]
+        }
+        var buttonPane = {
+            xtype: 'panel',
+            border: 0,
+            bodyStyle: {
+                background: 'lightGrey'
+            },
+            margin: {left: 10, right: 5, bottom: 15, top: 15},
+            items: [{
+                xtype: 'button',
+                iconCls: 'icon-delete'
+            }]
+        };
+        this.items = [imagePane, namePane, buttonPane];
+        this.callParent(arguments);
+    }
+
+
+})
