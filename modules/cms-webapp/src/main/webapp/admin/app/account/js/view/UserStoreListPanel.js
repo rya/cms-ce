794515c4044362f1113775303316b11679976306
Viktar Fiodarau <vfi@enonic.com>
2011-09-26 15:05:15 +0300

    B-02281: Admin 2.0: Accounts: User Wizard: create userstore list view (step 1)
     See spec: http://wiki.enonic.com/display/dev/Task+-+Admin+2.0+-+User+Wizard
    The User Wizard tasks should be done in the order they appear in the sprint.

--- modules/cms-webapp/src/main/webapp/admin/app/account/js/view/UserStoreListPanel.js
+++ modules/cms-webapp/src/main/webapp/admin/app/account/js/view/UserStoreListPanel.js
@@ -9,10 +9,16 @@ Ext.define('App.view.UserStoreListPanel', {
 
     initComponent: function() {
         var tpl = '<tpl for=".">' +
-                '<div class="userstore">' +
-                '<input type="radio" name="userstore" value="{key}">' +
-                '<p>{name}</p>' +
-                '</div>' +
+                    '<div class="userstore">' +
+                    '<input type="radio" name="userstore" value="{key}">' +
+                    '<div class="userstore-block">' +
+                        '<div class="left"><img width="50" height="50" src="app/account/images/app-icon-userstores.png"/></div>' +
+                        '<div class="center">' +
+                            '<h2>{name}</h2>' +
+                            '<p>(usersstores\\\\{name})</p>' +
+                        '</div>' +
+                    '</div>' +
+                    '</div><br>' +
                 '</tpl>';
         this.tpl = tpl;
         this.itemSelector = 'div.userstore'
