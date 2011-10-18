2f3c8bf82adec46d39a6cc669bf64312b50eb1f4
Sten Roger Sandvik <srs@enonic.com>
2011-10-18 23:58:12 +0200

    Fixed some integration tests. Must do more work here.

---
+++ modules/cms-itest/src/test/resources/com/enonic/cms/itest/config/MockGlobalConfig.java
@@ -0,0 +1,125 @@
+package com.enonic.cms.itest.config;
+
+import com.enonic.cms.core.config.GlobalConfig;
+import com.google.common.collect.Maps;
+import com.google.common.io.Files;
+
+import java.io.File;
+import java.util.Map;
+import java.util.Properties;
+
+public class MockGlobalConfig
+    implements GlobalConfig
+{
+    private final File homeDir = Files.createTempDir();
+
+    public File getHomeDir()
+    {
+        return this.homeDir;
+    }
+
+    public File getConfigDir()
+    {
+        return new File(getHomeDir(), "config");
+    }
+
+    public String getJdbcDialect()
+    {
+        return "auto";
+    }
+
+    public File getPluginConfigDir()
+    {
+        return new File(getHomeDir(), "plugins");
+    }
+
+    public File getPluginDeployDir()
+    {
+        return new File(getHomeDir(), "plugins");
+    }
+
+    public long getPluginScanPeriod()
+    {
+        return 0;
+    }
+
+    public File getCountriesFile()
+    {
+        return new File(getHomeDir(), "config/countries.xml");
+    }
+
+    public File getVirtualHostConfigFile()
+    {
+        return new File(getHomeDir(), "config/vhost.properties");
+    }
+
+    public boolean getJdbcLogging()
+    {
+        return false;
+    }
+
+    public boolean getJdbcConnectionTrace()
+    {
+        return false;
+    }
+
+    public int getTxDefaultTimeout()
+    {
+        return 1000;
+    }
+
+    public File getBlobStoreDir()
+    {
+        return new File(getHomeDir(), "data/blobs");
+    }
+
+    public int getAutoLoginTimeout()
+    {
+        return -1;
+    }
+
+    public String getAdminDefaultLanguage()
+    {
+        return "en";
+    }
+
+    public String getMainSmtpHost()
+    {
+        return "localhost";
+    }
+
+    public String getAdminEmail()
+    {
+        return "dummy@enonic.com";
+    }
+
+    public String getAdminPassword()
+    {
+        return "password";
+    }
+
+    public boolean getLivePortalTraceEnabled()
+    {
+        return false;
+    }
+
+    public int getLivePortalTraceLongestSize()
+    {
+        return 100;
+    }
+
+    public int getLivePortalTraceHistorySize()
+    {
+        return 100;
+    }
+
+    public Map<String, String> getMap()
+    {
+        return Maps.newHashMap();
+    }
+
+    public Properties getProperties()
+    {
+        return new Properties();
+    }
+}
---
+++ modules/cms-itest/src/test/resources/com/enonic/cms/itest/plugin/MockPluginManager.java
@@ -0,0 +1,94 @@
+package com.enonic.cms.itest.plugin;
+
+import com.enonic.cms.api.plugin.ext.Extension;
+import com.enonic.cms.api.plugin.ext.FunctionLibrary;
+import com.enonic.cms.api.plugin.ext.TaskHandler;
+import com.enonic.cms.api.plugin.ext.TextExtractor;
+import com.enonic.cms.api.plugin.ext.http.HttpAutoLogin;
+import com.enonic.cms.api.plugin.ext.http.HttpInterceptor;
+import com.enonic.cms.api.plugin.ext.http.HttpResponseFilter;
+import com.enonic.cms.core.plugin.ExtensionSet;
+import com.enonic.cms.core.plugin.PluginHandle;
+import com.enonic.cms.core.plugin.PluginManager;
+import com.google.common.collect.Lists;
+import com.sun.xml.internal.xsom.impl.scd.Iterators;
+import java.util.Iterator;
+import java.util.List;
+
+public class MockPluginManager
+    implements PluginManager
+{
+    public List<PluginHandle> getPlugins()
+    {
+        return Lists.newArrayList();
+    }
+
+    public ExtensionSet getExtensions()
+    {
+        return new ExtensionSet()
+        {
+            public List<Extension> getAllExtensions()
+            {
+                return Lists.newArrayList();
+            }
+
+            public List<FunctionLibrary> getAllFunctionLibraries() {
+                return Lists.newArrayList();
+            }
+
+            public List<HttpInterceptor> getAllHttpInterceptors() {
+                return Lists.newArrayList();
+            }
+
+            public List<HttpResponseFilter> getAllHttpResponseFilters() {
+                return Lists.newArrayList();
+            }
+
+            public List<HttpAutoLogin> getAllHttpAutoLoginPlugins() {
+                return Lists.newArrayList();
+            }
+
+            public List<TaskHandler> getAllTaskPlugins() {
+                return Lists.newArrayList();
+            }
+
+            public TaskHandler findTaskPlugin(String className) {
+                return null;
+            }
+
+            public FunctionLibrary findFunctionLibrary(String namespace) {
+                return null;
+            }
+
+            public List<HttpInterceptor> findMatchingHttpInterceptors(String path) {
+                return Lists.newArrayList();
+            }
+
+            public List<HttpResponseFilter> findMatchingHttpResponseFilters(String path) {
+                return Lists.newArrayList();
+            }
+
+            public HttpAutoLogin findMatchingHttpAutoLoginPlugin(String path) {
+                return null;
+            }
+
+            public TextExtractor findTextExtractorPluginByMimeType(String mimeType) {
+                return null;
+            }
+
+            public List<TextExtractor> getAllTextExtractorPlugins() {
+                return Lists.newArrayList();
+            }
+
+            public Iterator<Extension> iterator()
+            {
+                return Iterators.empty();
+            }
+        };
+    }
+
+    public PluginHandle findPluginByKey(long key)
+    {
+        return null;
+    }
+}
