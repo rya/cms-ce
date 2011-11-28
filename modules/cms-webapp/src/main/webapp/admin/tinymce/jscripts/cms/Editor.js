82cbdc7c94b2cb74f59a71eacc14f59b0c564020
Thomas Andersen <tan@enonic.com>
2011-11-28 10:40:49 +0100

    D-01886: Content form layout gets broken in readonly mode when using IE

--- modules/cms-webapp/src/main/webapp/admin/tinymce/jscripts/cms/Editor.js
+++ modules/cms-webapp/src/main/webapp/admin/tinymce/jscripts/cms/Editor.js
@@ -33,32 +33,22 @@ function Editor( id )
 
         Create a overlay for shading when the editor is disabled.
     */
+
     this.createOverlayForDisabledMode = function()
     {
-        var editor = tinyMCE.get(this.id);
-        var editorIframeElem = document.getElementById(this.id + '_ifr');
-        var editorBodyElem = editor.getBody();
-        var editorRectangle = editor.dom.getRect(editor.getContentAreaContainer());
-        var widthForOverlayElem = editorRectangle.w + 20;
-        var heightForOverlayElem = editorRectangle.h;
-        var overlayElem = editor.getDoc().createElement('div');
-        
-        editorBodyElem.style.overflow = 'hidden';
+        try {
+            var editor = tinyMCE.get(this.id);
+            var editorIframeElem = document.getElementById(this.id + '_ifr');
 
-        editorIframeElem.scrolling = 'no';
-        editorIframeElem.scrollbar = 'no';
-        editorIframeElem.style.overflow = 'hidden';
+            editorIframeElem.scrolling = 'no';
+            editorIframeElem.scrollbar = 'no';
 
-        overlayElem.style.backgroundColor = '#fff';
-        overlayElem.style.position = 'absolute';
-        overlayElem.style.width = widthForOverlayElem + 'px';
-        overlayElem.style.height = heightForOverlayElem + 'px';
-        overlayElem.style.left = '0';
-        overlayElem.style.top = '0';
-        overlayElem.setAttribute('mce_bogus', '1');
-
-        this.cmsutil.setOpacity(overlayElem, .5);
-
-        editorBodyElem.appendChild(overlayElem);
+            var editorBodyElem = editor.getBody();
+            this.cmsutil.setOpacity(editorBodyElem, .5);
+        }
+        catch(exception)
+        {
+        }
     };
+
 }
